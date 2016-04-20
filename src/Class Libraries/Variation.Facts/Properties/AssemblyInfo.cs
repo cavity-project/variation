6fa47a80e461fa7fafed30d86d3f558bae0a014b
adean <alan.dean@gmail.com>
2016-04-20 10:45:10 +0100

    Created skeleton solution.

---
+++ src/Class Libraries/Variation.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyDefaultAlias("Cavity.Variation.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Variation.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Variation Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Variation Facts Library (Release)")]
+
+#endif
---
+++ src/Class Libraries/Variation/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Variation.dll")]
+[assembly: AssemblyTitle("Cavity.Variation.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Variation Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Variation Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity", Justification = "This is a root namespace.")]
